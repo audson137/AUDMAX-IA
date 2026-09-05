af15893e78d94c1e96ad2c75eb42cd686f2cbee8
audson137 <audsonarparticipacoes@gmail.com>
2026-09-05 12:32:25 -0300

    Criar js/audmax-quantum-brain.js

---
+++ js/audmax-quantum-brain.js
@@ -0,0 +1,5 @@
+<!-- Seus scripts atuais permanecem aqui intactos -->
+<script src="js/suas-apis-antigas.js"></script>
+
+<!-- INCLUA O NOVO CÉREBRO DA V12 AQUI -->
+<script src="js/audmax-quantum-brain.js"></script>
